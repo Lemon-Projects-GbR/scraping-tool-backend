f64f391bf83021c65fa211c2a3c61fc9d7e9d0a9
diasdecastro <cristian.diass@hotmail.com>
2024-10-19 15:37:39 +0200

    revert

--- src/index.ts
+++ src/index.ts
@@ -12,34 +12,35 @@ const app = express();
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(bodyParser.json());
 
-app.get("/", async (req, res) => {
-  const data = await prepareHTMLContainer(
-    [
-      "https://sports.tipico.de/de/alle/1101/42301",
-      "https://sports.tipico.de/de/alle/1101/41301",
-    ],
-    ".SportsCompetitionsEvents-styles-module-competitions-events-block > .EventRow-styles-module-event-row"
-  );
+app.post("/", async (req, res) => {
+  console.log(req.body);
+  // const data = await prepareHTMLContainer(
+  //   [
+  //     "https://sports.tipico.de/de/alle/1101/42301",
+  //     "https://sports.tipico.de/de/alle/1101/41301",
+  //   ],
+  //   ".SportsCompetitionsEvents-styles-module-competitions-events-block > .EventRow-styles-module-event-row"
+  // );
 
-  const scheme = {
-    //NOTE: alles ohne $() ist ein fester Wert
-    bookie: "tipico",
-    competition: { country: "GER", name: "Bundesliga" },
-    game: {
-      link: "$('.EventTeams-styles-team-title').eq(0).text();",
-      date: "",
-      team1: "$('.EventTeams-styles-module-team-title').eq(0).text();",
-      team2: "$('.EventTeams-styles-module-team-title').eq(1).text();",
-      odds: {
-        team1: "",
-        draw: "",
-        team2: "",
-      },
-    },
-  };
+  // const scheme = {
+  //   //NOTE: alles ohne $() ist ein fester Wert
+  //   bookie: "tipico",
+  //   competition: { country: "GER", name: "Bundesliga" },
+  //   game: {
+  //     link: "$('.EventTeams-styles-team-title').eq(0).text();",
+  //     date: "",
+  //     team1: "$('.EventTeams-styles-module-team-title').eq(0).text();",
+  //     team2: "$('.EventTeams-styles-module-team-title').eq(1).text();",
+  //     odds: {
+  //       team1: "",
+  //       draw: "",
+  //       team2: "",
+  //     },
+  //   },
+  // };
 
-  const response = scrapeData(data, scheme);
-  res.send(response);
+  // const response = scrapeData(data, scheme);
+  // res.send(response);
 });
 
 app.post("/ai-search", async (req, res) => {
